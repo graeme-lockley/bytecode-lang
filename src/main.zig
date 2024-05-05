b2e738de48f18b58383f12a8035f3a40914c5576
Graeme Lockley <graeme.lockley@gmail.com>
2024-05-05 11:38:46 +0200

    feature: implement raw interpreter

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Interpreter = @import("./interpreter.zig");
+
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
@@ -16,12 +18,30 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    if (args.len == 4) {
-        try stdout.print("We are here.\n", .{});
+    if (args.len == 2) {
+        const fileName = args[1];
+
+        const buffer = try loadBinary(allocator, fileName);
+        defer allocator.free(buffer);
+
+        try Interpreter.eval(allocator, buffer);
+    } else if (args.len == 3) {
+        // const iterations = try std.fmt.parseInt(usize, args[1], 10);
+        unreachable;
     } else {
         try stdout.print("Error: Incorrect number of arguments.\n", .{});
 
-        try stdout.print("Usage: {s} [ast] iterations file\n", .{args[0]});
+        try stdout.print("Usage: {s} [iterations] file\n", .{args[0]});
         std.process.exit(1);
     }
+}
+
+fn loadBinary(allocator: std.mem.Allocator, fileName: []const u8) ![]u8 {
+    var file = try std.fs.cwd().openFile(fileName, .{});
+    defer file.close();
+
+    const fileSize = try file.getEndPos();
+    const buffer: []u8 = try file.readToEndAlloc(allocator, fileSize);
+
+    return buffer;
 }
